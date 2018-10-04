ef5404333a29e9d1a06ca87ccf08aef66ba357c1
Andy Brewer <ambrewer69@gmail.com>
2018-10-04 22:58:36 +0100

    fix to routing, removed redirectTo dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   { path: 'tree', component: TreePageComponent },
   {
     path: '',
-    redirectTo: '/dashboard',
+    component: DashboardComponent,
     pathMatch: 'full'
   },
   { path: '**', component: PageNotFoundComponent }
